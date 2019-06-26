6258b8bbea0de15d4e2e3bda866438e031a63c3b
Jean Daniel Boccara <jdboccara@gmail.com>
2019-06-26 21:17:36 +0200

    rajout budget 400K

---
+++ src/DataFixtures/BudgetFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+  namespace App\DataFixtures;
+
+
+  use App\Entity\Budget;
+  use App\Service\Slugify;
+  use Doctrine\Bundle\FixturesBundle\Fixture;
+  use Doctrine\Common\Persistence\ObjectManager;
+
+
+  class BudgetFixtures extends Fixture
+  {
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $budget = new Budget();
+        $budget->setMontant(400000);
+        $manager->persist($budget);
+        $manager->flush();
+    }
+
+
+  }
