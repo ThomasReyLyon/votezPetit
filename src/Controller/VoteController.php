64743cbd5b3f899536e8b5a8d355f2417ad9adf8
chikungugna <thomas.rey@posteo.net>
2019-06-27 11:05:30 +0200

    vote controller créé

---
+++ src/Controller/VoteController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * Class VoteController
+ * @package App\Controller
+ * @Route("/vote")
+ */
+class VoteController extends AbstractController
+{
+
+	public function vote()
+	{
+
+	}
+}
