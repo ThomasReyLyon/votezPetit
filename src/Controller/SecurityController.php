bf6e4f1ad9803b83911b31e8210ed66ae1bd5b71
Kev <Kevin_Heitz_student2019@wilder.school>
2019-06-27 11:56:29 +0200

    auth

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,15 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Citoyen;
-use App\Form\RegisterType;
-use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -29,38 +24,12 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * This is the route the user can use to logout.
-     *
-     * But, this will never be executed. Symfony will intercept this first
-     * and handle the logout automatically. See logout in config/packages/security.yaml
-     *
-     * @Route("/logout", name="app_logout")
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     * @throws Exception
      */
     public function logout()
     {
-        throw new \Exception('This should never be reached!');
-    }
-
-    /**
-     * @Route("/register", name="register")
-     */
-    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
-    {
-        $citoyen = new Citoyen();
-        $form =$this->createForm(RegisterType::class);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $hash = $encoder->encodePassword($citoyen, $citoyen->getPassword());
-            $citoyen->setPassword($hash);
-            
-            $manager->persist($citoyen);
-            $manager->flush();
-            $this->addFlash('success', 'yess');
-            return $this->redirectToRoute('app_login');
-        }
-
-        return $this->render('security/register.html.twig', [
-            'form' => $form->createView()
-            ]);
+        // controller can be blank: it will never be executed!
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
